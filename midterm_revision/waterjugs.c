4112a7e624839ee727de86e2c8ffdde049ec083e
Minh Nguyen <94669196+jethzgg@users.noreply.github.com>
2025-05-11 22:40:49 +0700

    Update waterjugs.c

--- midterm_revision/waterjugs.c
+++ midterm_revision/waterjugs.c
@@ -49,6 +49,73 @@ int main()
 {
     int a, b, c;
     scanf("%d %d %d", &a, &b, &c);
+    #include <stdio.h>
+
+int gcd(int a, int b)
+{
+    while (a != b)
+    {
+        if (a > b) a -= b;
+        else b -= a;
+    }
+    return a;
+}
+
+int pour(int a_max, int b_max, int c_max)
+{
+    int a = a_max, b = 0, count = 1;
+    while (a != c_max && b != c_max)
+    {
+        if ((b + a) > b_max)
+        {
+            a -= (b_max - b);
+            b = b_max;
+            count++;
+        }
+        else
+        {
+            b += a;
+            a = 0;
+            count++;
+        }
+        if (b == c_max || a == c_max)
+        {
+            return count;
+        }
+        if (a == 0)
+        {
+            count++;
+            a = a_max;
+        }
+        if (b == b_max)
+        {
+            b = 0; 
+            count++;
+        }
+    }
+    return count;
+}
+
+int main()
+{
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    if (a > b)
+    {
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+    if (c % gcd(a,b))
+    {
+        printf("-1");
+    }
+    else
+    {
+        printf("%d",pour(a,b,c));
+    }
+    return 0;
+}
     if (c % gcd(a,b))
     {
         printf("-1");
